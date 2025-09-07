fe534639f22e95c0b6e55e47454a4dc87d1f98bc
Shamko Aliaksandr <shamkoag@gmail.com>
2025-09-07 21:05:27 +0200

    feat: aa

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 module.exports = function towelSort(matrix) {
-  if (!matrix || matrix.length === 0) {
+  if (matrix === undefined) {
     return [];
   }
-  const result = [];
-  for (let i = 0; i < matrix.length; i += 1) {
-    if (i % 2 === 0) {
-      result.push(...matrix[i]);
-    } else {
-      result.push(...matrix[i].reverse());
-    }
+
+  if (matrix.length === 0) {
+    const result = [];
+    return result;
   }
-  return result;
+
+  if (matrix.length === 2) {
+    const arrOne = matrix[0];
+    const arrTwo = matrix[1].reverse();
+    const result = [...arrOne, ...arrTwo];
+    return result;
+  }
+
+  if (matrix.length === 3) {
+    const arrOne = matrix[0];
+    const arrTwo = matrix[1].reverse();
+    const arrThree = matrix[2];
+    const result = [...arrOne, ...arrTwo, ...arrThree];
+    return result;
+  }
+
+  if (matrix.length === 4) {
+    const arrOne = matrix[0];
+    const arrTwo = matrix[1].reverse();
+    const arrThree = matrix[2];
+    const arrFour = matrix[3].reverse();
+    const result = [...arrOne, ...arrTwo, ...arrThree, ...arrFour];
+    return result;
+  }
+  return [];
 };
